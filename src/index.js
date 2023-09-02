10cfba544efde54e63e28d0c35dee620152717fb
kaykakaraka <23278323+kaykakaraka@users.noreply.github.com>
2023-09-02 18:42:52 +0100

    Generating title using this.state and an object stored in the backend

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import vignettes from './objects/vignettes';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      vignettes: vignettes,
+      number: 0
+    }
+  }
+
+  generateTitle() {
+    let num = this.state.number;
+    let title = this.state.vignettes[num].title;
+    return title;
   }
 
   render() {
     return (
-      <h1>How To Go Outside / Now </h1>
+      <h1>{this.generateTitle()}</h1>
     )
   }
 }
---
+++ src/objects/vignettes.js
@@ -0,0 +1,13 @@
+const vignettes = [
+  {
+    title: 'how to be together / now'
+  }, {
+    title: 'Linda gets a girlfriend'
+  }, {
+    title: 'how to go outside'
+  }, {
+    title: 'how to go outside / now'
+  }
+]
+
+export default vignettes;
