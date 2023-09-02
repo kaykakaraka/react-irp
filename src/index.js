c4a42bd11552a0c65f263f1ef63a0a09607480ca
kaykakaraka <23278323+kaykakaraka@users.noreply.github.com>
2023-09-02 18:32:19 +0100

    Created App Component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Helloworld React!</h1>, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <h1>How To Go Outside / Now </h1>
+    )
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
