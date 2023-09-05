84b4a25b19952e44b7a912f45992e7ee46852110
kaykakaraka <23278323+kaykakaraka@users.noreply.github.com>
2023-09-05 10:56:32 +0100

    Nested VibrationButton within SceneOne so this can also be a parent component for Vibration

---
+++ src/components/sceneOne/sceneOne.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import VibrationButton from './vibrationButton'
+
+class SceneOne extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <VibrationButton/>
+    )
+  }
+}
+
+export default SceneOne;
---
+++ src/components/sceneOne/vibration.js
@@ -0,0 +1,9 @@
+class Vibration extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    <ul>Having a coke with you</ul>
+  }
+}
